e49c283ae44ca975ee89198bc139d5586eac73cf
BMarx981 <b.marx981@gmail.com>
2019-09-15 08:24:16 -0400

    Update flutter

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -9,11 +9,13 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage> {
-  List<Container> _getLinkPics() {
+  Future _getLinkPics() async {
     List<Container> list = [];
     if (widget.details['links']['flickr_images'] != null) {
+      print(widget.details['links']['flickr_images'][0]);
       for (var links in widget.details['links']['flickr_images']) {
         list.add(links);
+        print(links);
       }
     }
     return list;
@@ -31,107 +33,123 @@ class _DetailsPageState extends State<DetailsPage> {
 
   @override
   Widget build(BuildContext context) {
+//    _getLinkPics();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.details['mission_name']),
       ),
-      body: ListView(
-        children: <Widget>[
-          Hero(
-            tag: widget.details['mission_name'],
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 400.0,
-                width: 70.0,
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(.5),
-                  borderRadius: BorderRadius.circular(30.0),
-                  image: DecorationImage(
-                    image: NetworkImage(
-                        widget.details['links']['mission_patch_small']),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  'https://farm2.staticflickr.com/1486/25787998624_3ca213be1e_o.jpg'),
             ),
           ),
-          SizedBox(height: 20.0),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadiusDirectional.circular(20.0),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(18.0, 8.0, 0.0, 0.0),
-                    child: Container(
-                      child: Text(
-                        'Launched From:',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 25.0,
-                        ),
+          child: ListView(
+            children: <Widget>[
+              Hero(
+                tag: widget.details['mission_name'],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 400.0,
+                    width: 70.0,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(.5),
+                      borderRadius: BorderRadius.circular(30.0),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            widget.details['links']['mission_patch_small']),
+                        fit: BoxFit.fill,
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(18.0, 8.0, 18.0, 18.0),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadiusDirectional.circular(20.0),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        //Launch title
+                        padding: const EdgeInsets.fromLTRB(18.0, 8.0, 0.0, 0.0),
+                        child: Container(
+                          child: Text(
+                            'Launched From:',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 25.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        //Site name
+                        padding:
+                            const EdgeInsets.fromLTRB(18.0, 8.0, 18.0, 18.0),
+                        child: Text(
+                          widget.details['launch_site']['site_name_long'],
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        //Launch date local
+                        padding: const EdgeInsets.fromLTRB(18.0, 0.0, 8.0, 8.0),
+                        child: Container(
+                          child: Text(
+                            _getDate(widget.details['launch_date_local']),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 25.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[350],
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
                     child: Text(
-                      widget.details['launch_site']['site_name_long'],
-                      textAlign: TextAlign.left,
+                      widget.details['details'] != null
+                          ? widget.details['details']
+                          : 'No Details',
                       style: TextStyle(
+                        fontSize: 30.0,
                         color: Colors.black,
-                        fontSize: 20.0,
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(18.0, 0.0, 8.0, 8.0),
-                    child: Container(
-                      child: Text(
-                        _getDate(widget.details['launch_date_local']),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 25.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 20.0),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[350],
-                borderRadius: BorderRadius.circular(30.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Text(
-                  widget.details['details'] != null
-                      ? widget.details['details']
-                      : 'No Details',
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    color: Colors.black,
                   ),
                 ),
               ),
-            ),
+              SizedBox(height: 20.0),
+            ],
           ),
-          SizedBox(height: 20.0),
-        ],
+        ),
       ),
     );
   }
